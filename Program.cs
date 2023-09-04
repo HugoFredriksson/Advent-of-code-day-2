29658c122d7492bbfb327322ca6ceca02c688ff7
HugoFredriksson <hufr04001@utb.vaxjo.se>
2023-09-04 10:18:26 +0200

    del 2

--- Program.cs
+++ Program.cs
@@ -5,11 +5,14 @@ string[] input = File.ReadAllLines("input.txt");
 
 foreach (string inputItem in input)
 {
-    if (inputItem == "A Z"||inputItem == "B X" ||inputItem == "C Y") totalScore += 0;
-    if (inputItem == "A X" || inputItem == "B Y" || inputItem == "C Z") totalScore += 3;
-    if (inputItem == "A Y" || inputItem == "B Z" || inputItem == "C X") totalScore += 6;
-    if (inputItem[2] == 'Z') totalScore += 3;
-    if (inputItem[2] == 'Y') totalScore += 2; 
-    if (inputItem[2] == 'X') totalScore += 1;
+    //Påse
+    if (inputItem == "A Z" || inputItem == "C X" || inputItem == "B Y") totalScore += 2;
+    //Sten
+    if (inputItem == "B X" || inputItem == "A Y" || inputItem == "C Z") totalScore += 1;
+    //Sax
+    if (inputItem == "C Y" || inputItem == "B Z" || inputItem == "A X") totalScore += 3;
+    if (inputItem[2] == 'Z') totalScore += 6;
+    if (inputItem[2] == 'Y') totalScore += 3;
+    if (inputItem[2] == 'X') totalScore += 0;
 }
 Console.WriteLine(totalScore);
